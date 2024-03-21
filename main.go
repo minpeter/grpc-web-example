2c0b4f9076ccbeb9f742340d21a6314312f3bb2f
minpeter <minpeter@friendli.ai>
2024-03-21 17:49:45 +0900

    Refactor code for improved performance and readability

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	pb "github.com/minpeter/grpc-web-example/helloworld"
+
+	"google.golang.org/grpc"
+)
+
+var (
+	port = flag.Int("port", 9090, "The server port")
+)
+
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
+
+// server streaming call SayRepeatHello
+func (s *server) SayRepeatHello(in *pb.RepeatHelloRequest, stream pb.Greeter_SayRepeatHelloServer) error {
+	log.Printf("Received: %v", in.GetName())
+	for i := 0; i < 10; i++ {
+		if err := stream.Send(&pb.HelloReply{Message: "Hello " + in.GetName() + fmt.Sprintf(" %d", i)}); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func main() {
+
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
